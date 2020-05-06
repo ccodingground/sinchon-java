5f27d817407594c4a6fa46669db271a284f2b736
nick2615 <57625736+nick2615@users.noreply.github.com>
2020-05-06 14:15:49 +0900

    자바 첫번째 파일

---
+++ Test01.java
@@ -0,0 +1,12 @@
+
+public class Test01 {
+//jvm
+	public static void main(String args[]) {
+		System.out.println("안녕하세요");
+		System.out.println("Hello World! ");
+		System.out.println("aaaa");
+		System.out.println("bbbb");
+		
+	}
+
+}
